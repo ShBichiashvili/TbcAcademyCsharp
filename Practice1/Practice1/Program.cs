7b8a9fc1044ff1ff58e9992245a1541f8cd52997
ShBichiashvili <bichiashvilii.shotiko@gmail.com>
2023-06-05 16:43:13 +0400

    Weather practice

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,32 @@
+﻿int temperature = 0;
+
+while (true)
+{
+    Console.Write("Write temperature: ");
+
+    if (IsValidInputStr(Console.ReadLine()))
+    {
+        Console.WriteLine(CalculateTemperatureStr(temperature));
+        break;
+    }
+}
+
+bool IsValidInputStr(string? inputStr)
+{
+    if (int.TryParse(inputStr, out temperature))
+        return true;
+
+    return false;
+}
+
+string CalculateTemperatureStr(int temperature)
+{
+    if (temperature < 0)
+        return "It's freezing";
+    else if (temperature >= 0 && temperature <= 30)
+        return "It's good weather";
+    else
+    {
+        return "It's hot";
+    }
+}
