79ebddd90680ab835da939ba77ee5a1788858a80
ShBichiashvili <bichiashvilii.shotiko@gmail.com>
2023-06-05 17:37:53 +0400

    Jeirani practice

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -1,32 +1,146 @@
-﻿int temperature = 0;
+﻿//// Practice 1
+//int temperature = 0;
 
-while (true)
+//while (true)
+//{
+//    Console.Write("Write temperature: ");
+
+//    if (IsValidInputStr(Console.ReadLine()))
+//    {
+//        Console.WriteLine(CalculateTemperatureStr(temperature));
+//        break;
+//    }
+//}
+
+//bool IsValidInputStr(string? inputStr)
+//{
+//    if (int.TryParse(inputStr, out temperature))
+//        return true;
+
+//    return false;
+//}
+
+//string CalculateTemperatureStr(int temperature)
+//{
+//    if (temperature < 0)
+//        return "It's freezing";
+//    else if (temperature >= 0 && temperature <= 30)
+//        return "It's good weather";
+//    else
+//    {
+//        return "It's hot";
+//    }
+//}
+
+
+string? inputStr;
+bool startGame = true;
+
+while (startGame)
 {
-    Console.Write("Write temperature: ");
+    Console.WriteLine("Options:\n1. stone\n2. scissors\n3. paper\n");
+    Console.Write("Write your world: ");
+    inputStr = Console.ReadLine();
 
-    if (IsValidInputStr(Console.ReadLine()))
-    {
-        Console.WriteLine(CalculateTemperatureStr(temperature));
-        break;
+	if (!IsUserInputValid(inputStr))
+	{
+        Console.WriteLine("Incorrect word!!!\n");
+    }
+
+	else
+	{
+		string? computerStr = GenerateComputerTurn();
+
+        Console.WriteLine($"User: {inputStr} - {computerStr} Computer");
+
+		if (IsDraw(inputStr, computerStr))
+		{
+			Console.WriteLine("Game result: Draw!");
+		}
+
+		else if (IsUserWon(inputStr, computerStr))
+		{
+			Console.WriteLine("Game result: User wons!");
+		}
+
+		else if (IsComputerWon(inputStr, computerStr))
+		{
+			Console.WriteLine("Game result: Computer wons!");
+		}
+
+		else
+		{
+            Console.WriteLine("Error game proccess");
+        }
+
+        Console.Write("\nPlay again? y/n: ");
+		if (Console.ReadLine().Equals("y"))
+			continue;
+		else
+		{
+            Console.WriteLine("\nEnd game...");
+            break;
+		}
     }
 }
 
-bool IsValidInputStr(string? inputStr)
+bool IsUserInputValid(string? inputStr)
 {
-    if (int.TryParse(inputStr, out temperature))
+	switch (inputStr)
+	{
+		case "stone":
+			return true;
+		case "scissors":
+			return true;
+		case "paper":
+			return true;
+
+		default:
+			return false;
+	}
+}
+
+// If user wons - returns true, else return false
+string? GenerateComputerTurn()
+{
+    Random random = new Random();
+
+	switch (random.Next(0, 2))
+	{
+		case 0:
+			return "stone";
+		case 1:
+			return "scissors";
+		case 2:
+			return "paper";
+
+		default:
+			return null;
+    }
+}
+
+bool IsUserWon(string? userStr, string? computerStr)
+{
+	if (userStr.Equals("stone") && computerStr.Equals("scissors"))
+		return true;
+	else if (userStr.Equals("scissors") && computerStr.Equals("paper"))
+		return true;
+	else if (userStr.Equals("paper") && computerStr.Equals("stone"))
+		return true;
+
+	return false;
+}
+
+bool IsComputerWon(string? userStr, string? computerStr)
+{
+    if (computerStr.Equals("stone") && userStr.Equals("scissors"))
+        return true;
+    else if (computerStr.Equals("scissors") && userStr.Equals("paper"))
+        return true;
+    else if (computerStr.Equals("paper") && userStr.Equals("stone"))
         return true;
 
     return false;
 }
 
-string CalculateTemperatureStr(int temperature)
-{
-    if (temperature < 0)
-        return "It's freezing";
-    else if (temperature >= 0 && temperature <= 30)
-        return "It's good weather";
-    else
-    {
-        return "It's hot";
-    }
-}
+bool IsDraw(string? userStr, string? computerStr) => userStr.Equals(computerStr);
